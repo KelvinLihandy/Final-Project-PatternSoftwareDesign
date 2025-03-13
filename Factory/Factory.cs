a7606d4eff2483a07dba22f02ba3a78149ca3e05
KelvinLihandy <kelvinlihandy2005@gmail.com>
2025-03-13 21:29:28 +0700

    template push

---
+++ Factory/Factory.cs
@@ -0,0 +1,32 @@
+﻿using FinalProjectPSD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectPSD.Factory
+{
+    public class Factory
+    {
+        public static MsJewel createJewel(int brandId, int categoryId, string name, int price, int releaseYear)
+        {
+            MsJewel newJewel = new MsJewel();
+            newJewel.BrandID = brandId;
+            newJewel.CategoryID = categoryId;
+            newJewel.JewelName = name;
+            newJewel.JewelPrice = price;
+            newJewel.JewelReleaseYear = releaseYear;
+            return newJewel;
+        }
+        public static MsUser createUser(string name, string pass, string email, DateTime dob, string gender, string role)
+        {
+            MsUser newUser = new MsUser();
+            newUser.UserName = name;
+            newUser.UserPassword = pass;
+            newUser.UserEmail = email;
+            newUser.UserDOB = dob;
+            newUser.UserGender = gender;
+            return newUser;
+        }
+    }
+}
---
+++ Repository/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using FinalProjectPSD.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectPSD.Repository
+{
+    public class Singleton
+    {
+        private DatabaseJewel db;
+
+        public DatabaseJewel GetDatabase()
+        {
+            if (db == null)
+            {
+                db = new DatabaseJewel();
+            }
+            return db;
+        }
+    }
+}
